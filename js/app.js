c1dcca37db7e1bea8c9c18adc31317b9dbd81a3d
MADHVI-PATHAK <81677802+MADHVI-PATHAK@users.noreply.github.com>
2021-10-01 14:57:49 +0530

    add initial p.1 files

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const navSlide = () =>{
+    const bars = document.querySelector('.fa-bars');
+    const web = document.querySelector('.web');
+
+    bars.addEventListener('click', () => {
+        web.classList.toggle('web-active');
+    });
+}
+navSlide();
+
+const openSearch = () =>{
+    const icon = document.getElementById("myOverlay");
+    const insert = document.querySelector('.insert');
+    insert.addEventListener('click', () => {
+        icon.style.display = "block";
+    })
+}
+openSearch();
+
+const closeSearch = () =>{
+    const icon = document.getElementById("myOverlay");
+    const close = document.querySelector('.closebtn');
+    close.addEventListener('click', () => {
+        icon.style.display = "none";
+    })
+}
+closeSearch();
